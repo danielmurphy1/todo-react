748d3121ae5c032085caee9c6ab3285b12ed990c
Dan <daniel.murphy.1@gmail.com>
2020-12-23 20:14:43 -0500

    fixed addTodo bug

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Form (props){
     return(
-        <form className="container" onSubmit={props.addExpense}>
+        <form className="container" onSubmit={props.addTodo}>
             <div className="input-group">
                 <input type="text" name="todoText" value={props.state.toDoText} onChange={props.handleChange} className="form-control" placeholder="Enter To Do Item Here"></input>
                 <button className="btn btn-dark ml-3" type="submit" >Add ToDo</button>
