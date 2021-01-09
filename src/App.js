07752e1570ed7ebe605a133af6412974434c8cea
Dan <daniel.murphy.1@gmail.com>
2021-01-08 19:24:31 -0500

    fixed setting state bug

--- src/App.js
+++ src/App.js
@@ -40,40 +40,41 @@ class App extends React.Component {
     })
   }
 
-  //another way to update and delete, but does not remove from local storage since uses prevState
-
-  // updateTodo(todoID, isChecked) {
-  //   this.setState(prevState => {      
-  //     const matchingTodo = prevState.todos.find(todo => todo.id === todoID);
-  //     const matchingTodoIndex = prevState.todos.indexOf(matchingTodo);
-  //     prevState.todos[matchingTodoIndex].isChecked = isChecked;
-  //     return prevState;
-  //   })
-  // }
-
-  // handleDelete(todoID){
-  //   this.setState(prevState => {
-  //     const todos = prevState.todos.filter(todo => todo.id !==todoID);
-  //     prevState.todos = todos
-  //     return prevState
-  //   })
-  // }
+  updateTodo(todoID, isChecked) {
+    this.setState(prevState => {      
+      const matchingTodo = prevState.todos.find(todo => todo.id === todoID);
+      const matchingTodoIndex = prevState.todos.indexOf(matchingTodo);
+      prevState.todos[matchingTodoIndex].isChecked = isChecked;
+      return prevState;
+    })
+  }
 
   handleDelete(todoID){
-    const todos = this.state.todos.filter(todo => todo.id !==todoID);
-    this.setState({
-      todos : todos
+    this.setState(prevState => {
+      const todos = prevState.todos.filter(todo => todo.id !==todoID);
+      
+      return {todos}
+      
     })
   }
 
-  updateTodo(todoID, isChecked) {
-    const matchingTodo = this.state.todos.find(todo => todo.id === todoID);
-    const matchingTodoIndex = this.state.todos.indexOf(matchingTodo);
-    this.state.todos[matchingTodoIndex].isChecked = isChecked;
-    this.setState({
-      todos : this.state.todos
-    })
-  }
+  //another way to update and delete, but the updateToDo updates state directly...
+
+  // handleDelete(todoID){
+  //   const todos = this.state.todos.filter(todo => todo.id !==todoID);
+  //   this.setState({
+  //     todos : todos
+  //   })
+  // }
+
+  // updateTodo(todoID, isChecked) {
+  //   const matchingTodo = this.state.todos.find(todo => todo.id === todoID);
+  //   const matchingTodoIndex = this.state.todos.indexOf(matchingTodo);
+  //   this.state.todos[matchingTodoIndex].isChecked = isChecked;
+  //   this.setState({
+  //     todos : this.state.todos
+  //   })
+  // }
 
   componentDidMount(){
     localStorage.getItem("todos") && this.setState({
